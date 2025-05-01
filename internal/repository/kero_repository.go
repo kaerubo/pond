0aa8ad56d7808fa2fb1c6144c9314516975ad453
z6wdc <195cho@gmail.com>
2025-05-01 19:36:47 +0900

    kero repository interface

---
+++ internal/repository/kero_repository.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/z6wdc/pond/internal/entity"
+)
+
+type KeroSaver interface {
+	Save(ctx context.Context, k *entity.Kero) error
+}
+
+type KeroFinder interface {
+	FindByID(ctx context.Context, id int64) (*entity.Kero, error)
+	FindAll(ctx context.Context) ([]*entity.Kero, error)
+}
+
+type KeroUpdater interface {
+	Update(ctx context.Context, k *entity.Kero) error
+}
+
+type KeroDeleter interface {
+	Delete(ctx context.Context, id int64) error
+}
