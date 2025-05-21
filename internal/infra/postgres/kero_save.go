4e1684072b19372fe5ca41b8e768f8842fd7e34b
z6wdc <195cho@gmail.com>
2025-05-21 16:19:54 +0900

    WIP

--- internal/infra/postgres/kero_save.go
+++ internal/infra/postgres/kero_save.go
@@ -2,12 +2,22 @@ package postgres
 
 import (
 	"context"
+	"database/sql"
 	"github.com/kaerubo/pond/internal/db/models"
 	"github.com/kaerubo/pond/internal/entity"
+	"github.com/kaerubo/pond/internal/repository"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
-func (r *keroRepository) Save(ctx context.Context, k *entity.Kero) error {
+type KeroSave struct {
+	db *sql.DB
+}
+
+func NewKeroSave(db *sql.DB) repository.KeroSaver {
+	return &KeroSave{db: db}
+}
+
+func (s *KeroSave) Save(ctx context.Context, k *entity.Kero) error {
 	model := &models.Kero{
 		ID:        k.ID,
 		Title:     k.Title,
@@ -16,5 +26,5 @@ func (r *keroRepository) Save(ctx context.Context, k *entity.Kero) error {
 		UpdatedAt: k.UpdatedAt,
 	}
 
-	return model.Insert(ctx, r.db, boil.Infer())
+	return model.Insert(ctx, s.db, boil.Infer())
 }
